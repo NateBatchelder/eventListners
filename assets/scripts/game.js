cbcce485581f23edbf7778cb141b08f614a853e8
Nathan Batchelder <natebatchelder@gmail.com>
2021-10-24 13:34:20 -0700

    adding logic to the game JS

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -3,4 +3,18 @@ export default class Game {
         this.turn = "X";
         this.board = new Array(9).fill(null); // 9 is the length of the array    
     }
+
+    nextTurn() {
+        this.turn = this.turn === "X" ? "O" : "X";
+    }
+    
+    makeMove(i) {
+        // adds protection if the user tries to make a move on a cell that is already occupied
+        if (this.board[i]) {
+            return;
+        }
+        // adds the move to the board, and then changes the turn to the next player
+        this.board[i] = this.turn;
+        this.nextTurn();    
+    }
 }
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -3,5 +3,9 @@ import Game from "./Game.js";
 let game = new Game();
 
 console.log(game.turn);
-console.log(game.board);
+game.makeMove(0);
+console.log(game.board.join(","));
+console.log(game.turn);
+game.makeMove(6);
+console.log(game.board.join(","));
 
