a23be78cf2c4e452d4a5b8b2543cb9a08a572d70
Nathan Batchelder <natebatchelder@gmail.com>
2021-10-24 16:51:49 -0700

    status now prints to screen

--- assets/scripts/gameView.js
+++ assets/scripts/gameView.js
@@ -1,1 +1,21 @@
-gameView
+// exports the GameView object
+export default class GameView {
+    constructor(root) {
+        // uses the root element to create an element 'GameView'
+        this.root = root;
+        // uses innerHTML to create elements: turn, game status, and reset
+        this.root.innerHTML = `
+            <div class="header">
+                <div class="header__turn">
+                    X's turn
+                </div>
+                <div class="header__status">
+                    In Progress
+                </div>
+                <button type="button" class="header__reset">
+                    <i class="material-icons">refresh</i>
+                </button>
+            </div>
+        `;
+    }
+}
