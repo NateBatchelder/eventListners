3c0c7a976b5042d55798f13dc3a9eccd4dbf762c
Nathan Batchelder <natebatchelder@gmail.com>
2021-10-24 19:34:01 -0700

    styling to CSS and game functionality to JS

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -8,11 +8,20 @@ let gameView = new GameView(document.getElementById("app"));
 // define functions
 gameView.onTileClick = function (i) {
     console.log(`Array tile ${i} clicked.`);
+    game.makeMove(i);
+    gameView.update(game);
 };
 
-gameView.onRestartClick = function (event) {
- 
+gameView.onRestartClick = function () {
+    game = new Game();
+    gameView.update(game);
+    console.log("Restart button clicked, game is restarted!");
+
 };
+
+// update the gameView with the game data
+gameView.update(game);
+ 
 
 
 
